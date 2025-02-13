943b96db8d82675026f75cdf734d51eba7dd926c
tristan14-cloud <tristan.siregar14@gmail.com>
2025-02-13 23:01:40 +0700

    Add files via upload

---
+++ 177 Patterns_ CSS & Tailwind_files/config.js
@@ -0,0 +1,7 @@
+(function() {
+  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
+  window._POSTHOG_REMOTE_CONFIG['phc_m9DPye2qU1H4ymd2DQMz4uiH3E1RFEIS1E7bq8IioRE'] = {
+    config: {"token": "phc_m9DPye2qU1H4ymd2DQMz4uiH3E1RFEIS1E7bq8IioRE", "supportedCompression": ["gzip", "gzip-js"], "hasFeatureFlags": false, "captureDeadClicks": false, "capturePerformance": false, "autocapture_opt_out": true, "autocaptureExceptions": false, "analytics": {"endpoint": "/i/v0/e/"}, "elementsChainAsString": true, "sessionRecording": false, "heatmaps": false, "surveys": [], "defaultIdentifiedOnly": true},
+    siteApps: []
+  }
+})();
